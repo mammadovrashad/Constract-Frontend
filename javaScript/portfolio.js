ca5e2bfb043c63c07fdc5df9cfaef3eaa7bbc0e7
Resad Memmedov <talehg2932@gmail.com>
2020-12-19 22:25:32 +0400

    sticky ve portfolio hissesinin yazilmasi

--- javaScript/portfolio.js
+++ javaScript/portfolio.js
@@ -1,9 +1,32 @@
 let subMenuPortfolio=document.querySelector(".sub-menuPortfolio")
-console.log(subMenuPortfolio)
 let li2=document.querySelectorAll('.li')[2]
-console.log(li2)
 li2.addEventListener('mouseover',function(){
     subMenuPortfolio.style.display="block"
 })
 li2.addEventListener('mouseout',function(){
     subMenuPortfolio.style.display="none"})
+
+
+let largeblockSticky=document.querySelector('.largeblock-section3')
+let logoSticky=document.querySelectorAll("img")[0]
+let submenuPortfolio=document.querySelector(".sub-menuPortfolio")
+let portfolioMenu=document.querySelector(".menu-section3")
+console.log(portfolioMenu)
+window.addEventListener('scroll',function(){
+    if(window.scrollY>300){
+    largeblockSticky.style.position='fixed'
+    largeblockSticky.style.maxHeight=75+'px';
+    logoSticky.style.maxHeight=75+'px'
+    subMenuPortfolio.style.top=67+'px'
+    largeblockSticky.style.background='white'
+    }
+    else{
+        largeblockSticky.style.position='absolute'
+        largeblockSticky.style.maxHeight=130+'px';
+        logoSticky.style.maxHeight=120+'px'
+        subMenuPortfolio.style.top=93+'px'
+        largeblockSticky.style.background='white'
+        portfolioMenu.style.marginTop=17+'px'
+       
+    }
+})
